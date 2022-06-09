aed68aa58a5b15bf4593b02082b445ec2674c9a2
Wahid Ullah <wahid.ullah.cse@gmail.com>
2022-06-09 16:50:26 +0600

    module store update

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::resource('category', 'CategoryController');
-
-
-Route::get('{path}', 'HomeController@index');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//
+//Route::resource('category', 'CategoryController');
+//
+//
+//Route::get('{path}', 'HomeController@index');
 
 
 
